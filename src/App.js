96a1a83de26397799e2bdfe0475dab33bcc614e9
THRIVENI-H-N <107698532+THRIVENI-H-N@users.noreply.github.com>
2022-09-30 12:12:35 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import ClassComponent from './components/ClassComponents';
+import './components/style.css'
+
+function App() {
+  return (
+    <div className="App">
+      <ClassComponent />
+    </div>
+  );
+}
+
+export default App;
